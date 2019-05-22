92c078ae804b5eb594a300cb7fa4e4634e2f82b0
Justin Herzog <justinzog@gmail.com>
2019-05-22 17:18:11 -0600

    Finished Rock Paper Scissors. I might switch the AI back tho this one might suck, we'll see after ballred tries it

--- RockPaperScissors/RockPaperScissors/Controller/GameViewController.swift
+++ RockPaperScissors/RockPaperScissors/Controller/GameViewController.swift
@@ -12,12 +12,8 @@ class GameViewController: UIViewController {
     
     var lastPlayedArray: [Int] = []
     
-    var totalCount: Double {
-        return rockCount + paperCount + scissorsCount
-    }
-    var rockCount: Double = 0
-    var paperCount: Double = 0
-    var scissorsCount: Double = 0
+    var totalRounds: Int = 0
+    
     var currentScore: Int = 0
     var highScore: Int = 0
     var usersNumber: Int = -1
@@ -27,22 +23,37 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var highScoreLabel: UILabel!
     
-    @IBOutlet weak var youWinLabel: UILabel!
+    @IBOutlet weak var yourChoiceImage: UIImageView!
+    
+    @IBOutlet weak var compsChoiceImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        youWinLabel.isHidden = true
+        yourChoiceImage.image = nil
+        compsChoiceImage.image = nil
     }
     
     func compChoose() {
-        if totalCount < 10 {
-            let setCompsNumber: Int = Int.random(in: 0...2)
-            compsNumber = setCompsNumber
-        } else {
-            let rockRatio = ((rockCount / totalCount) * 100)
-            let paperRatio = ((paperCount / totalCount) * 100)
-            // let scissorRatio = ((scissorsCount / totalCount) * 100)
+        var rocks: Double = 0
+        var papers: Double = 0
+        var scissors: Double = 0
+        
+        for i in lastPlayedArray {
+            if i == 0 {
+                rocks += 1
+            } else if i == 1 {
+                papers += 1
+            } else if i == 2 {
+                scissors += 1
+            }
+        }
+        
+        if lastPlayedArray.count == 30 {
+            let rockRatio = ((rocks / 23) * 100)
+            let paperRatio = ((papers / 23) * 100)
+            // let scissorsRatio = ((scissors / 23) * 100)
             let compsTempNumber: Double = Double.random(in: 1...100)
+            
             if compsTempNumber <= rockRatio {
                 compsNumber = 1
             } else if rockRatio < compsTempNumber && compsTempNumber <= (rockRatio + paperRatio) {
@@ -50,21 +61,9 @@ class GameViewController: UIViewController {
             } else if (rockRatio + paperRatio) < compsTempNumber && compsTempNumber <= 100 {
                 compsNumber = 0
             }
-        }
-    }
-    
-    func winLabelAnimation() {
-        let scale = CGAffineTransform(scaleX: 5, y: 5)
-        let unscale = CGAffineTransform(scaleX: 1/50, y: 1/50)
-        youWinLabel.isHidden = false
-        UIView.animate(withDuration: 0.33, animations: {
-            self.youWinLabel.transform = scale
-        }) { (true) in
-            UIView.animate(withDuration: 0.33, animations: {
-                self.youWinLabel.transform = unscale
-            }) { (true) in
-                self.youWinLabel.isHidden = true
-            }
+        } else if lastPlayedArray.count < 30 {
+            let setCompsNumber: Int = Int.random(in: 0...2)
+            compsNumber = setCompsNumber
         }
     }
     
@@ -74,48 +73,33 @@ class GameViewController: UIViewController {
         case 0:
             switch compsNumber {
             case 0:
-                youWinLabel.text = "You Tied"
-                winLabelAnimation()
+                return
             case 1:
-                youWinLabel.text = "You Lose"
                 currentScore -= 1
-                winLabelAnimation()
             case 2:
-                youWinLabel.text = "You Win"
                 currentScore += 1
-                winLabelAnimation()
             default:
                 print("Something went wrong")
             }
         case 1:
             switch compsNumber {
             case 0:
-                youWinLabel.text = "You Win"
                 currentScore += 1
-                winLabelAnimation()
             case 1:
-                youWinLabel.text = "You Tied"
-                winLabelAnimation()
+                return
             case 2:
-                youWinLabel.text = "You Lose"
                 currentScore -= 1
-                winLabelAnimation()
             default:
                 print("Something went wrong")
             }
         case 2:
             switch compsNumber {
             case 0:
-                youWinLabel.text = "You Lose"
                 currentScore -= 1
-                winLabelAnimation()
             case 1:
-                youWinLabel.text = "You Win"
                 currentScore += 1
-                winLabelAnimation()
             case 2:
-                youWinLabel.text = "You Tied"
-                winLabelAnimation()
+                return
             default:
                 print("Something went wrong")
             }
@@ -123,27 +107,56 @@ class GameViewController: UIViewController {
             print("User didn't choose button")
         }
         currentScoreLabel?.text = String("Current Score: \(currentScore)")
-        usersNumber = -1
+    }
+    
+    func updateLastPlayedArray() {
+        if lastPlayedArray.count == 30 {
+            lastPlayedArray.remove(at: 0)
+            lastPlayedArray.append(usersNumber)
+            usersNumber = -1
+        } else if lastPlayedArray.count < 30 {
+            lastPlayedArray.append(usersNumber)
+            usersNumber = -1
+        }
+    }
+    
+    func setCompsImage() {
+        if compsNumber == 0 {
+            compsChoiceImage.image = UIImage(named: "Rock")
+        } else if compsNumber == 1 {
+            compsChoiceImage.image = UIImage(named: "Paper")
+        } else if compsNumber == 2 {
+            compsChoiceImage.image = UIImage(named: "Scissors")
+        }
     }
     
     @IBAction func rockButtonTapped(_ sender: Any) {
         usersNumber = 0
-        rockCount += 1
         changeScore()
-        print(totalCount)
+        updateLastPlayedArray()
+        yourChoiceImage.image = UIImage(named: "Rock")
+        setCompsImage()
+        totalRounds += 1
+        print(totalRounds)
     }
     
     @IBAction func paperButtonTapped(_ sender: Any) {
         usersNumber = 1
-        paperCount += 1
         changeScore()
-        print(totalCount)
+        updateLastPlayedArray()
+        yourChoiceImage.image = UIImage(named: "Paper")
+        setCompsImage()
+        totalRounds += 1
+        print(totalRounds)
     }
     
     @IBAction func scissorsButtonTapped(_ sender: Any) {
         usersNumber = 2
-        scissorsCount += 1
         changeScore()
-        print(totalCount)
+        updateLastPlayedArray()
+        yourChoiceImage.image = UIImage(named: "Scissors")
+        setCompsImage()
+        totalRounds += 1
+        print(totalRounds)
     }
 }
